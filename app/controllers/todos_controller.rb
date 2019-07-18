7bebd51c774448f765aad95f1e4ab237e24b18f9
Alejandro <alejodiaz.123@gmail.com>
2019-07-18 00:50:24 -0400

    Avance hasta Edit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,43 @@
+class TodosController < ApplicationController
+    def index
+        @todos = Todo.all
+    end
+
+    def new
+        @todo = Todo.new
+    end
+    
+
+    def create
+        @todos = Todo.new(todo_params)
+        @todos.save
+
+        if @todos.save == true
+            redirect_to todos_path
+        else
+            alert "Ingrese los campos correctramente"
+            redirect_to todos_new_path
+        end
+    end
+
+    def show
+        @todo = Todo.find(params[:id])
+    end
+
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+        @todo.update(todo_params)
+        redirect_to todos_path
+    end
+    
+
+    private
+    def todo_params
+        params.require(:todo).permit(:description, :completed) 
+    end
+    
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'todos', to: 'todos#index'
+  get '/todos/new', to: 'todos#new'
+  post 'todos', to: 'todos#create'
+  get '/todos/:id', to: 'todos#show', as: 'todo'
+  get '/todos/:id', to: 'todos#edit', as: 'edit'
+  patch 'todos/:id', to: 'todos#update'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
